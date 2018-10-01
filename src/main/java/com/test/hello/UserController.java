a1ed507cb4f6f4e3e6c32c7d99cf6074c4ba9a19
Alex <pavlovshura@gmail.com>
2018-10-01 19:24:41 +0300

    Изменение маппинга UserController
    Signed-off-by: Alex <pavlovshura@gmail.com>

--- src/main/java/com/test/hello/UserController.java
+++ src/main/java/com/test/hello/UserController.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     private static int count;
 
-    @RequestMapping("/user")
+    @RequestMapping("/new")
     public User greeting(@RequestParam(name="name", required=false, defaultValue="Username") String name) {
         count++;
         return new User(count, name);
+
     }
 
 }
