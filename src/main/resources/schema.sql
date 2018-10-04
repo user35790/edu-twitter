0a4817e26641db8fc88c58645dc88495026858b5
Alex <pavlovshura@gmail.com>
2018-10-04 13:34:45 +0300

    Обновление БД

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,42 @@
-CREATE TABLE Users(
-id int NOT NULL,
-name varchar(30),
-PRIMARY KEY(id)
+CREATE TABLE users(
+id int NOT NULL AUTO_INCREMENT,
+login varchar (30) NOT NULL,
+password varchar (30) NOT NULL,
+
+name varchar (100),
+age int,
+sex boolean,
+date_start date,
+
+status varchar (50),
+image varchar (300),
+
+PRIMARY KEY (id)
+);
+
+
+CREATE TABLE tweets(
+id int NOT NULL AUTO_INCREMENT,
+id_creator int NOT NULL,
+
+text varchar (300) NOT NULL,
+
+date date,
+likes int,
+
+PRIMARY KEY (id)
+);
+
+
+CREATE TABLE comments(
+id int NOT NULL AUTO_INCREMENT,
+id_creator int NOT NULL,
+id_tweet int NOT NULL,
+
+text varchar (100) NOT NULL,
+
+date date,
+likes int,
+
+PRIMARY KEY (id)
 );
