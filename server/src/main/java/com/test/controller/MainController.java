e65ba3f553d226f613e1acee4e3e2a2db6362658
Alex <pavlovshura@gmail.com>
2018-10-16 22:15:01 +0300

    Удалена сущность Message

--- server/src/main/java/com/test/controller/MainController.java
+++ server/src/main/java/com/test/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.test.controller;
 
-import com.test.domain.Message;
-import com.test.repos.MessagesRepo;
+import com.test.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,51 +12,16 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    @Autowired
-    private MessagesRepo messagesRepo;
-
     @GetMapping("/hello")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> map) {
+        map.put("name", name);
         return "hello";
     }
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting() {
         return "main";
-    }
-
-    @GetMapping("/messages")
-    public String messages(Map<String, Object> model){
-        Iterable<Message> messages = messagesRepo.findAll();
-        model.put("messages", messages);
-        return "messages";
-    }
-
-    @PostMapping("/messages")
-    public String add(@RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String, Object> model){
-        Message message = new Message(text, tag);
-
-        messagesRepo.save(message);
-
-        Iterable<Message> messages = messagesRepo.findAll();
-        model.put("messages", messages);
-        return "messages";
-    }
-
-    @PostMapping("/messages/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()) {
-            messages = messagesRepo.findByTag(filter);
-        } else{
-            messages = messagesRepo.findAll();
-        }
-            model.put("messages", messages);
-
-        return "messages";
     }
 }
