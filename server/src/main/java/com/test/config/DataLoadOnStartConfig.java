cdcf7b9ee9667efea57200d88b11c24848c230e9
Alex <pavlovshura@gmail.com>
2018-10-17 00:54:27 +0300

    Начальные данные для БД

--- server/src/main/java/com/test/config/DataLoadOnStartConfig.java
+++ server/src/main/java/com/test/config/DataLoadOnStartConfig.java
@@ -1,7 +1,9 @@
 package com.test.config;
 
+import com.test.model.Tweet;
 import com.test.model.User;
 import com.test.model.UserRole;
+import com.test.repos.TweetRepo;
 import com.test.repos.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +12,31 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 
 @Component
-public class DataConfig {
+public class DataLoadOnStartConfig {
 
     @Autowired
     private UsersRepo usersRepo;
 
+    @Autowired
+    private TweetRepo tweetRepo;
+
     @Bean
     public String loadData(){
-        usersRepo.save(new User("a", "a", true, Collections.singleton(UserRole.USER)));
+        User user_admin = new User("admin", "admin", true, Collections.singleton(UserRole.USER));
+        User user_a = new User("a", "a", true, Collections.singleton(UserRole.USER));
+        User user_b = new User("b", "b", true, Collections.singleton(UserRole.USER));
+        User user_c = new User("c", "c", true, Collections.singleton(UserRole.USER));
+        usersRepo.save(user_admin);
+        usersRepo.save(user_a);
+        usersRepo.save(user_b);
+        usersRepo.save(user_c);
+        tweetRepo.save(new Tweet(user_admin, "test"));
+        tweetRepo.save(new Tweet(user_admin, "test2"));
+        tweetRepo.save(new Tweet(user_a, "a1"));
+        tweetRepo.save(new Tweet(user_a, "a2"));
+        tweetRepo.save(new Tweet(user_b, "b1"));
+        tweetRepo.save(new Tweet(user_c, "c1"));
+
         return "ok, bro";
     }
 }
